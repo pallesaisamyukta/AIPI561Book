0458d8a29360b8dc049b77c6db569fb7e8581b29
Sai Samyukta Palle <141570219+pallesaisamyukta@users.noreply.github.com>
2024-08-08 08:06:32 -0400

    Update test_app.py

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Ensure the app.py file can be found
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 import unittest
-from ..app import app
 
 class FlaskAppTests(unittest.TestCase):
     def setUp(self):
