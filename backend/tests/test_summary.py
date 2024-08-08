04af3197d62263c461c76311eff0db7ffee73714
Sai Samyukta Palle <141570219+pallesaisamyukta@users.noreply.github.com>
2024-08-08 08:16:42 -0400

    Update test_summary.py

--- backend/tests/test_summary.py
+++ backend/tests/test_summary.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import unittest
 from unittest.mock import patch, MagicMock
-from ../summary import Summarizer
+from summary import Summarizer
 
 class SummarizerTests(unittest.TestCase):
     def setUp(self):
